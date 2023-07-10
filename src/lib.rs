a93598cf0a612414ca672e0c8c370c52ade40904
Tobias Hunger <tobias.hunger@slint-ui.com>
2023-07-10 15:54:14 +0200

    The humble beginnings

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+// Copyright © Tobias Hunger <tobias.hunger@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pub mod commands;
+pub mod printer;
+
---
+++ src/printer.rs
@@ -0,0 +1,158 @@
+// Copyright © Tobias Hunger <tobias.hunger@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use std::cell::RefCell;
+use std::io::Write;
+
+#[derive(Clone, Debug, Default, PartialEq, PartialOrd, clap::ValueEnum)]
+pub enum LogLevel {
+    Off,
+    Error,
+    #[default]
+    Warn,
+    Info,
+    Debug,
+    Trace,
+}
+
+pub struct Printer {
+    log_level: LogLevel,
+    headlines: RefCell<Vec<String>>,
+    lock: RefCell<std::io::StdoutLock<'static>>,
+}
+
+impl Printer {
+    pub fn new(log_level: &LogLevel) -> Self {
+        let result = Self {
+            log_level: log_level.clone(),
+            headlines: RefCell::new(Default::default()),
+            lock: RefCell::new(std::io::stdout().lock()),
+        };
+        result.print_status();
+        result
+    }
+
+    fn print_status(&self) {
+        write!(
+            self.lock.borrow_mut(),
+            "{} {}",
+            ansi_term::Style::new().on(ansi_term::Colour::Blue).fg(ansi_term::Colour::White).paint("Processing >>> "),
+            self.headlines.borrow().join(" / ")
+        )
+        .unwrap();
+        std::io::stdout().flush().unwrap();
+    }
+
+    fn clear_line(&self) {
+        write!(self.lock.borrow_mut(), "{}\r", ansi_escapes::EraseLine).unwrap();
+    }
+
+    fn print_formatted(&self, prefix: &str, message: &str) {
+        self.clear_line();
+        writeln!(self.lock.borrow_mut(), "{prefix} {message}").unwrap();
+
+        self.print_status();
+    }
+
+    fn print_headline(&self, level: usize, quiet: bool, prefix: &str, message: &str) {
+        let mut headlines = self.headlines.take();
+        let cut_off = level.saturating_sub(1);
+        assert!(headlines.len() >= cut_off);
+        headlines.resize(cut_off, Default::default());
+        headlines.push(message.to_string());
+        self.headlines.replace(headlines);
+
+        if !quiet {
+            self.print_formatted(prefix, message);
+        }
+    }
+
+    #[allow(unused)]
+    pub fn h1(&self, message: &str, quiet: bool) {
+        self.print_headline(
+            1,
+            quiet,
+            &format!("\n\n{}", ansi_term::Colour::Red.paint("******")),
+            &format!("{}", ansi_term::Style::new().bold().paint(message)),
+        );
+    }
+
+    #[allow(unused)]
+    pub fn h2(&self, message: &str, quiet: bool) {
+        self.print_headline(
+            2,
+            quiet,
+            &format!("\n{}", ansi_term::Colour::Red.paint("++++++")),
+            message,
+        );
+    }
+
+    #[allow(unused)]
+    pub fn h3(&self, message: &str, quiet: bool) {
+        self.print_headline(
+            3,
+            quiet,
+            &format!("{}", ansi_term::Colour::Green.paint("------")),
+            message,
+        );
+    }
+
+    #[allow(unused)]
+    pub fn error(&self, message: &str) {
+        if self.log_level >= LogLevel::Error {
+            self.print_formatted(
+                &format!("{}:", ansi_term::Colour::Red.paint("ERROR")),
+                message,
+            );
+        }
+    }
+
+    #[allow(unused)]
+    pub fn warn(&self, message: &str) {
+        if self.log_level >= LogLevel::Warn {
+            self.print_formatted(
+                &format!("{}:", ansi_term::Colour::Yellow.paint(" WARN")),
+                message,
+            );
+        }
+    }
+
+    #[allow(unused)]
+    pub fn info(&self, message: &str) {
+        if self.log_level >= LogLevel::Info {
+            self.print_formatted(
+                &format!("{}:", ansi_term::Colour::Blue.paint(" INFO")),
+                message,
+            );
+        }
+    }
+
+    #[allow(unused)]
+    pub fn debug(&self, message: &str) {
+        if self.log_level >= LogLevel::Debug {
+            self.print_formatted(
+                &format!("{}:", ansi_term::Colour::Cyan.paint("DEBUG")),
+                message,
+            );
+        }
+    }
+
+    #[allow(unused)]
+    pub fn trace(&self, message: &str) {
+        if self.log_level >= LogLevel::Trace {
+            self.print_formatted("TRACE:", message);
+        }
+    }
+
+    #[allow(unused)]
+    pub fn print(&self, message: &str) {
+        self.print_formatted("     :", message);
+    }
+}
+
+impl Drop for Printer {
+    fn drop(&mut self) {
+        self.clear_line();
+        writeln!(self.lock.borrow_mut(), "     : Done").unwrap();
+    }
+}
